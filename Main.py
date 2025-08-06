9110c7b361d87735476a47b3641647dd63aab5e0
agusberardinelli <berardinelliagustin@gmail.com>
2025-08-06 12:30:50 +0000

    commit

--- Main.py
+++ Main.py
@@ -1,3 +1,30 @@
-b=2
-a=1
-print(a+b)
+#Historias para elegir:
+
+#Historia 1
+#La historia se sitúa en el pueblo ficticio de Hawkins, en el estado de Indiana, Estados Unidos, durante los años ochenta,
+#cuando un niño de doce años llamado Will Byers desaparece misteriosamente.
+#Poco después, Eleven, una niña aparentemente fugitiva y con poderes telequinéticos,
+#se encuentra con Mike, Dustin y Lucas, amigos de Will, y los ayuda en la búsqueda de su amigo desaparecido.
+#Historia 2
+#La serie sigue la histoia de [Johnny] [Lawrence], exestudiante del [dojo] cobra kai,
+#quien busca la redencion reabriendo el infame dojo y reavivando, en el proceso,
+#su rivalidad con un ahora exitoso [Daniel] [LaRusso]. 
+#Este enemigo ha estado luchando por mantener el equilibrio
+#en su vida sin la guia de su mentor, el señor Miyagi
+#La serie trata sobre dos hombres que enfrentan sus demonios del pasado y 
+#resuelven las frustaciones del presente de la unica manera
+#en que saben hacerlo: a traves del karate.
+nombrePrinc =  str(input("Dame el nombre del personaje principal de la historia "))
+apellidoPrinc =  str(input("Dame el apellido del personaje principal de la historia "))
+dojo = str(input("dame el dojo de la historia"))
+nombreH = str(input("dame el nombre del heroe de la historia"))
+apellidoH = str(input("dame el nombre del heroe de la historia"))
+
+print(f"La serie sigue la histoia de {nombrePrinc} {apellidoPrinc} exestudiante del dojo {dojo}")
+print(f"quien busca la redencion reabriendo el infame dojo y reavivando, en el proceso")
+print(f"su rivalidad con un ahora exitoso {nombreH} {apellidoH}")
+print(f"Este enemigo ha estado luchando por mantener el equilibrio")
+print(f"en su vida sin la guia de su mentor, el señor Miyagi")
+print(f"La serie trata sobre dos hombres que enfrentan sus demonios del pasado y")
+print(f"resuelven las frustaciones del presente de la unica manera")
+print(f"en que saben hacerlo: a traves del karate.")
