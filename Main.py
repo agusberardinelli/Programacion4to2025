e3ad6ac8f6938b4c6d280132f3c08f61a83d777a
agusberardinelli <berardinelliagustin@gmail.com>
2025-08-06 08:51:13 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,18 @@
-b=2
-a=1
-print(a+b)
+# Historia 2
+# La serie sigue la histoia de Johnny Lawrence, exestudiante del dojo cobra kai,
+# quien busca la redencion reabriendo el infame dojo y reavivando, en el proceso,
+# su rivalidad con un ahora exitoso Daniel LaRusso. 
+# Este enemigo ha estado luchando por mantener el equilibrio
+# en su vida sin la guia de su mentor, el señor Miyagi
+# La serie trata sobre dos hombres que enfrentan sus demonios del pasado y 
+# resuelven las frustaciones del presente de la unica manera
+# en que saben hacerlo: a traves del karate.
+nombrePrinc =  str(input("Dame el nombre del personaje principal de la historia "))
+apellidoPrinc =  str(input("Dame el apellido del personaje principal de la historia "))
+dojo = str(input("dame el dojo de la historia"))
+nombreH = str(input("dame el nombre del heroe de la historia"))
+apellidoH = str(input("dame el nombre del heroe de la historia"))
+
+print(f"La serie sigue la histoia de {nombrePrinc} {apellidoPrinc} exestudiante del dojo {dojo}")
+print(f"su rivalidad con un ahora exitoso {nombreH} {apellidoH}")
+print()
