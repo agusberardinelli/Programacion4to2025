ed751ea9aa5fd330fff8578c17a1ac2afd421390
agusberardinelli <berardinelliagustin@gmail.com>
2025-09-08 18:21:34 +0000

    ACTIVIDAD FINAL

---
+++ ejerciciosDH/Condicionales/Act1.py
@@ -0,0 +1,6 @@
+edad = int(input( "Ingresar edad "))
+if edad >= 16 :
+  print("Tienes permitido conducir")
+else: 
+  print("No tienes permitido conducir")
+    
---
+++ ejerciciosDH/Condicionales/Act2.py
@@ -0,0 +1,13 @@
+gustoHelado = input("¿Qué sabor de helado deseas? ")
+
+# Comprobamos con condicionales
+if gustoHelado == "chocolate":
+    print("Sí, hay " + gustoHelado)
+elif gustoHelado == "vainilla":
+    print("Sí, hay " + gustoHelado)
+elif gustoHelado == "fresa":
+    print("Sí, hay " + gustoHelado)
+elif gustoHelado == "pistacho":
+    print("Sí, hay " + gustoHelado)
+else:
+    print("No hay " + gustoHelado)
